5f32ddd024e0cc67d7ab8dd0c1f6dd01734074cd
Deep_Js <sarma.d3e@gmail.com>
2023-02-07 19:27:48 +0530

    Revert "CRUD-IN-ANGULAR"
    This reverts commit 5ac5503a3cd7c7dd9bb1ade6eae92a94f2e73da4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSlideToggleModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { MatSlideToggleChange } from '@angular/material/slide-toggle';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { EmployesDashboardComponent } from './employes-dashboard/employes-dashboard.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import {  HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmployesDashboardComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    MatGridListModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employes-dashboard/employee-dashboard.modal.ts
+++
@@ -1,8 +0,0 @@
-export class EmployesDashboardModal {
-   id:number=0;
-   firstName:string='';
-   lastName:string='';
-   email:string='';
-   mobile:string='';
-   salary:string='';
-}
