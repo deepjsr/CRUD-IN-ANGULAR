5ac5503a3cd7c7dd9bb1ade6eae92a94f2e73da4
Deep_Js <sarma.d3e@gmail.com>
2023-02-07 12:49:42 +0530

    CRUD-IN-ANGULAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { EmployesDashboardComponent } from './employes-dashboard/employes-dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {  HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployesDashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
